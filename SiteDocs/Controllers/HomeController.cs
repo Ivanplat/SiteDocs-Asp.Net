b901cdf94d016c977b0a07170cc911cead9a7332
Ivan Platonov <EventGraphDev@gmail.com>
2023-07-31 17:43:31 +0300

    Add project files.

---
+++ SiteDocs/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteDocs.Models;
+
+namespace SiteDocs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult IndexClass()
+        {
+            return View();
+        }
+        public IActionResult IndexMoreClass()
+        {
+            return View();
+        }
+        public string AcceptResult(int code, string title, string issuedBy)
+        {
+            Doc doc = new Doc()
+            {
+                Code = code,
+                Title = title,
+                IssuedBy = issuedBy
+            };
+            return $"Код документа: {doc.Code}. Наименование: {doc.Title}. Кем выдан: {doc.IssuedBy}.";
+        }
+        public string AcceptResultClass(Doc doc)
+        {
+            return $"Код документа: {doc.Code}. Наименование: {doc.Title}. Кем выдан: {doc.IssuedBy}.";
+        }
+        public string AcceptResultMoreClass(Doc[] docs)
+        {
+            string result = string.Empty;
+            foreach (Doc doc in docs) 
+            {
+                result += $"Код документа: {doc.Code}. Наименование: {doc.Title}. Кем выдан: {doc.IssuedBy}.\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ SiteDocs/Models/Doc.cs
@@ -0,0 +1,10 @@
+﻿namespace SiteDocs.Models
+{
+    public class Doc
+    {
+        public int Code { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public string IssuedBy { get; set; } = string.Empty;
+
+    }
+}
